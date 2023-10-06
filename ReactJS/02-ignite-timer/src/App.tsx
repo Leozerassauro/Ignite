293abdd4953b3fe427c589b4727b6fc52ba7dd7f
Leonardo <leo.girardi97@hotmail.com>
2023-10-06 10:16:23 -0300

    added global colors

---
+++ ReactJS/02-ignite-timer/src/App.tsx
@@ -0,0 +1,14 @@
+import { ThemeProvider } from "styled-components"
+import { GlobalStyle } from "./styles/global"
+import { darkTheme } from "./styles/themes/dark"
+
+export function App() {
+
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <h1>Hello World</h1>
+
+      <GlobalStyle />
+    </ThemeProvider>
+  )
+}
