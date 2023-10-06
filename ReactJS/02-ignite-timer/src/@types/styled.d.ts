f9772ca52818dd157fb9815370f3f9a385b156df
Leonardo <leo.girardi97@hotmail.com>
2023-10-06 10:22:36 -0300

    added eslint

--- ReactJS/02-ignite-timer/src/@types/styled.d.ts
+++ ReactJS/02-ignite-timer/src/@types/styled.d.ts
@@ -2,7 +2,7 @@ import 'styled-components'
 
 import { darkTheme } from '../styles/themes/dark'
 
-type ThemeType = typeof darkTheme;
+type ThemeType = typeof darkTheme
 
 declare module 'styled-components' {
   export interface DefaultTheme extends ThemeType {}
